6491ab7c77ca3a615b34fedc924af30d05263c31
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-09-08 16:17:59 +0200

    fixed grid display for event cards on mainpage. Eventcards can now span full width, added extra breakpoint

--- src/components/UpcomingEventsSection.tsx
+++ src/components/UpcomingEventsSection.tsx
@@ -47,7 +47,8 @@ export default function UpcomingEvents() {
                 UPCOMING<br />ACTIVITIES
               </h2>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-10">
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-10">
                 {events.map((event) => (
                     <EventCard key={event.id} event={event} />
                 ))}
@@ -66,7 +67,6 @@ export default function UpcomingEvents() {
               </button>
               </>
               )}
-              </div>
               </div>
         </section>
     );
